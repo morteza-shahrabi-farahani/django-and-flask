a1e1413f43885906130668a2734fb8fd787dea65
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2022-02-07 12:22:02 +0330

    initial commit for new project

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 class Collection(models.Model):
@@ -11,6 +12,7 @@ class Promotion(models.Model):
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(default='-')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
