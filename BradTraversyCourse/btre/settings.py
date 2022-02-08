328831c7344f59d818a77e52872581459a3e776c
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2022-02-08 12:15:13 +0330

    connecting postgresql to django and adding some data to database

--- BradTraversyCourse/btre/settings.py
+++ BradTraversyCourse/btre/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost'
     }
 }
 
@@ -125,6 +128,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
