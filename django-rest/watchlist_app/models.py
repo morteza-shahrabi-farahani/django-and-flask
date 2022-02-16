2940ba2d574c8b162a2d7fa340b8e721bb347eaf
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2022-02-16 15:58:45 +0330

    add some spi without using  django rest

--- django-rest/watchlist_app/models.py
+++ django-rest/watchlist_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    
+def __str__(self):
+    return self.name
